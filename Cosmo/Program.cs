83e84a8ef7d5004ff69aa1ae431c84c68d1881a5
Miroslav <gljivar@gmail.com>
2023-07-30 23:27:23 +0200

    Add CosmosDb test project

---
+++ Cosmo/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Azure.Cosmos;
+using Azure.Identity;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// New instance of CosmosClient class
+using CosmosClient client = new(
+    accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT"),
+    tokenCredential: new DefaultAzureCredential()
+);
